69e2b3478642b28a959069ca498cb773df3aa7c0
“jverd” <jverd@clinflows.com>
2018-11-30 12:25:15 +0100

    Generate models classes

---
+++ shareloc_api/src/model/BeneficiaryId.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/*
+ * Class which permit to represent keys in the beneficiary table
+ */
+@Embeddable
+public class BeneficiaryId implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="id_achieved_service")
+	private int idAchievedService;
+	
+	@Column(name="id_user")
+	private int idUser;
+	
+	public BeneficiaryId() {
+		
+	}
+	
+	public BeneficiaryId(int service, int user) {
+		this.idAchievedService = service;
+		this.idUser = user;	
+	}
+	
+	public int getIdAchievedService() {
+		return idAchievedService;
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idAchievedService;
+		result = prime * result + idUser;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BeneficiaryId other = (BeneficiaryId) obj;
+		if (idAchievedService != other.idAchievedService)
+			return false;
+		if (idUser != other.idUser)
+			return false;
+		return true;
+	}
+}
---
+++ shareloc_api/src/model/Collocation.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the COLLOCATION database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Collocation.findAll", query="SELECT c FROM Collocation c")
+public class Collocation implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="id_collocation")
+	private int idCollocation;
+
+	@Column(name="name_collocation")
+	private String nameCollocation;
+
+	public Collocation() {
+	}
+
+	public int getIdCollocation() {
+		return this.idCollocation;
+	}
+
+	public void setIdCollocation(int idCollocation) {
+		this.idCollocation = idCollocation;
+	}
+
+	public String getNameCollocation() {
+		return this.nameCollocation;
+	}
+
+	public void setNameCollocation(String nameCollocation) {
+		this.nameCollocation = nameCollocation;
+	}
+
+}
