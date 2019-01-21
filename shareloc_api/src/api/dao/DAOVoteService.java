18f905fb3f2ab21b69b5551b1357722840978d57
Stevy Palarski <palarski@iutrs.unistra.fr>
2019-01-21 20:11:13 +0100

    Finalized api

---
+++ shareloc_api/src/api/dao/DAOVoteService.java
@@ -0,0 +1,11 @@
+package api.dao;
+
+import api.model.VoteService;
+
+public class DAOVoteService extends DAOAbstractFacade<VoteService>{
+
+	public DAOVoteService() {
+		super(VoteService.class);
+	}
+
+}
---
+++ shareloc_api/src/api/path/VotePath.java
@@ -0,0 +1,5 @@
+package api.path;
+
+public class VotePath {
+
+}
