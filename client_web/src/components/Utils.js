eeb0782a955db75c6a9500a9af792555d9134f62
Joffrey VERD <jverd@protonmail.com>
2019-01-23 19:00:56 +0100

    Cleaned the code

--- client_web/src/components/Utils.js
+++ client_web/src/components/Utils.js
@@ -1,12 +1,8 @@
 export function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response;
-    } else {
-        return response.text()
-        .then((text) => {
-            let error = new Error(text);
-            error.response = response;
-            throw error;
-        });
-    }
+    return new Promise((resolve, reject) => {
+        if (response.status >= 200 && response.status < 300) {
+            return resolve(response);
+        }
+        return reject(response.text());
+    });
 }
--- client_web/src/components/httpMethods.js
+++ client_web/src/components/httpMethods.js
@@ -1,19 +1,43 @@
+import { checkStatus } from './Utils';
+
+const API_HOST = 'http://jsonplaceholder.typicode.com';
+
+function request(route, method, body) {
+    let options = {
+        method: method,
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+    return fetch(API_HOST + route, options)
+        .then((result) => {
+            return checkStatus(result)
+            .then((res) => {
+                return res.json();
+            }).
+            catch ((error) => {
+                console.log(error);
+            })
+        }   
+        ).catch ( error => {
+            console.log(error);
+        })
+}
+
+export default {
+    get: (route) => request(route, 'GET'),
+    post: (route, body) => request(route, 'POST', body),
+    put: (route, body) => request(route, 'PUT', body),
+    delete: (route) => request(route, 'DELETE')
+}
+
 export function getMethod(route) {
     return fetch(route, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify()
     })
-}
-
-export function postMethod(route) {
-    alert('post method');
-}
-
-export function putMethod(route) {
-    alert('put method');
-}
-
-export function deleteMethod(route) {
-    alert('delete method');
 }
